304696ce8544c145206d4e5c0cc2838f6782ecfd
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-07 17:54:32 -0500

    cleaned things up, organized it into a class

--- One-Time_Pad.py
+++ One-Time_Pad.py
@@ -1,35 +1,39 @@
 import binascii
 
-key = int('0001010', 2) #sample 7-digit binary string
+class OneTimePad:
+	"""A simple toy cryptosystem"""
+	def __init__(self, key):
+		self.key = int(key, 2) #sample 7-digit binary string
+		self.test_message = "Hello! How are you doing today?"
 
-message = "Hello! How are you doing today?"
+	def Encode(self, string):
+		return ' '.join(str(int(format(ord(x)))^self.key) for x in string)
 
-encrypted_message = 'Boffe+*Be}*kxo*se*necdm*~enks5'
+	def Return_Int_List(self, space_separated_string):
+		string_list = space_separated_string.split(' ')
+		num_list = []
+		for num in string_list:
+			num_list.append(int(num))
+		return num_list
 
-def Encode(string):
-	return ' '.join(str(int(format(ord(x)))^key) for x in string)
+	def Get_Encoded_String(self, string):
+		num_list = self.Return_Int_List(string)
+		return ''.join(chr(x) for x in num_list)
 
-def Return_Int_List(space_separated_string):
-	string_list = space_separated_string.split(' ')
-	num_list = []
-	for num in string_list:
-		num_list.append(int(num))
-	return num_list
+	def Get_Chars(self, encrypted_message):
+		return " ".join([str(ord(x)) for x in encrypted_message])
 
-def Get_Encoded_String(string):
-	num_list = Return_Int_List(string)
-	return ''.join(chr(x) for x in num_list)
+	def Decode(self, string):
+		num_list = Return_Int_List(string)
+		decoded_list = [x^self.key for x in num_list]
+		return "".join(map(chr, decoded_list))
 
-def Get_Chars(encrypted_message):
-	return " ".join([str(ord(x)) for x in encrypted_message])
+	def Encrypt_Message(self, plain_text):
+		return self.Get_Encoded_String(self.Encode(plain_text))
 
-def Decode(string):
-	num_list = Return_Int_List(string)
-	decoded_list = [x^key for x in num_list]
-	return "".join(map(chr, decoded_list))
+	def Decrypt_Message(self, encrypted_message):
+		return self.Decode(self.Get_Chars(encrypted_message))
 
-def Encrypt_Message(plain_text):
-	return Get_Encoded_String(Encode(plain_text))
+Cryptosystem = OneTimePad('0001010')
 
-def Decrypt_Message(encrypted_message):
-	return Decode(Get_Chars(encrypted_message))
+print Cryptosystem.Encrypt_Message("My name is Steven Schmatz and this is my original message.")
